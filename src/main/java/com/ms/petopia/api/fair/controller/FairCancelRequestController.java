ae7e114f73510fbeb420a1e04b72cc1adc1888cc
awelf <1541lsh@gmail.com>
2026-08-06 16:56:44 +0900

    Fix: 행사 취소 신청 API 경로 변경으로 매핑 충돌 해소

--- src/main/java/com/ms/petopia/api/fair/controller/FairCancelRequestController.java
+++ src/main/java/com/ms/petopia/api/fair/controller/FairCancelRequestController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/fairs/{fairId}/cancel-requests")
+@RequestMapping("/api/fairs/{fairId}/fair-cancel-requests")
 @RequiredArgsConstructor
 public class FairCancelRequestController {
 
